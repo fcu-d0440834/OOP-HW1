865836a1c896a9e1ad00cf58cd9fa23031f8ea2e
user <user@FC12>
2016-09-30 09:57:33 +0800

    Homework 1

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -1,10 +1,13 @@
 package fcu.iecs.oop;
 
 public class Hello {
- 
+
 	public static void main(String[] args) {
-		
-		 System.out.println("Hello OOP");
+
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		lyrics = lyrics.replaceAll("it","\"her\"");
+		lyrics = lyrics.replaceAll("Cannot","Can't");
+		System.out.println(lyrics);
 	}
-	
+
 }
